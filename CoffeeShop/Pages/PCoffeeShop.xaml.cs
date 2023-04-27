9f47093cba35c4782e12447e11dc34ac98a43a9c
c3n9 <arseniy11041104@gmail.com>
2023-04-28 02:21:18 +0300

    buy coffee

--- CoffeeShop/Pages/PCoffeeShop.xaml.cs
+++ CoffeeShop/Pages/PCoffeeShop.xaml.cs
@@ -21,6 +21,7 @@ namespace CoffeeShop.Pages
     /// </summary>
     public partial class PCoffeeShop : Page
     {
+        Coffee contextCoffee;
         public PCoffeeShop()
         {
             InitializeComponent();
@@ -39,28 +40,130 @@ namespace CoffeeShop.Pages
 
         private void BBuy_Click(object sender, RoutedEventArgs e)
         {
-            var selectedCoffee = (sender as Button).DataContext as Coffee;
-            if (selectedCoffee == null)
+            contextCoffee = (sender as Button).DataContext as Coffee;
+            if (contextCoffee == null)
             {
                 return;
             }
-            TBName.Text = selectedCoffee.Name;
-            TBCost.Text = "$"+Convert.ToString(selectedCoffee.Cost);
-            ISelectedCoffee.Source = new BitmapImage(new Uri(selectedCoffee.Image, UriKind.RelativeOrAbsolute));
+            TBName.Text = contextCoffee.Name;
+            TBCost.Text = Convert.ToString(contextCoffee.Cost);
+            ISelectedCoffee.Source = new BitmapImage(new Uri(contextCoffee.Image, UriKind.RelativeOrAbsolute));
+            FullCost.Text = Convert.ToString(contextCoffee.Cost);
         }
 
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
-            if(TBCount.Text == "0")
+            if (contextCoffee == null)
+            {
+                MessageBox.Show("Select coffee");
+                return;
+            }
+            if (TBCount.Text == "0")
             {
                 return;
             }
             TBCount.Text = Convert.ToString(int.Parse(TBCount.Text) - 1);
+            FullCost.Text = Convert.ToString(double.Parse(FullCost.Text) - 0.1);
         }
 
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
+            if (contextCoffee == null)
+            {
+                MessageBox.Show("Select coffee");
+                return;
+            }
             TBCount.Text = Convert.ToString(int.Parse(TBCount.Text) + 1);
+            FullCost.Text = Convert.ToString(double.Parse(FullCost.Text) + 0.1);
+        }
+
+        private void Minus1cent_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBCount1cent.Text != "0" && !(double.Parse(TBYourMoney.Text) < 0.1))
+            {
+                TBCount1cent.Text = Convert.ToString(double.Parse(TBCount1cent.Text) - 1);
+                TBYourMoney.Text = Convert.ToString(double.Parse(TBYourMoney.Text) - 0.1);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Plus1Cent_Click(object sender, RoutedEventArgs e)
+        {
+            TBCount1cent.Text = Convert.ToString(double.Parse(TBCount1cent.Text) + 1);
+            TBYourMoney.Text = Convert.ToString(double.Parse(TBYourMoney.Text) + 0.1);
+        }
+
+        private void Minus5cent_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBCount5cent.Text != "0" && !(double.Parse(TBYourMoney.Text) < 0.5))
+            {
+                TBCount5cent.Text = Convert.ToString(double.Parse(TBCount5cent.Text) - 1);
+                TBYourMoney.Text = Convert.ToString(double.Parse(TBYourMoney.Text) - 0.5);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Plus5Cent_Click(object sender, RoutedEventArgs e)
+        {
+            TBCount5cent.Text = Convert.ToString(double.Parse(TBCount5cent.Text) + 1);
+            TBYourMoney.Text = Convert.ToString(double.Parse(TBYourMoney.Text) + 0.5);
+        }
+
+        private void Minus25cent_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBCount25cent.Text != "0" && !(double.Parse(TBYourMoney.Text) < 0.25))
+            {
+                TBCount25cent.Text = Convert.ToString(double.Parse(TBCount25cent.Text) - 1);
+                TBYourMoney.Text = Convert.ToString(double.Parse(TBYourMoney.Text) - 0.25);
+            }
+            else
+            {
+                return;
+            }
+           
+        }
+
+        private void Plus25Cent_Click(object sender, RoutedEventArgs e)
+        {
+            TBCount25cent.Text = Convert.ToString(double.Parse(TBCount25cent.Text) + 1);
+            TBYourMoney.Text = Convert.ToString(double.Parse(TBYourMoney.Text) + 0.25);
+        }
+
+        private void BBuyCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            if (contextCoffee == null)
+            {
+                MessageBox.Show("Select coffee");
+                return;
+            }
+            if (double.Parse(TBYourMoney.Text) < double.Parse(FullCost.Text))
+            {
+                MessageBox.Show("Not enough means");
+                return;
+            }
+            else if (double.Parse(TBYourMoney.Text) == double.Parse(FullCost.Text))
+            {
+                MessageBox.Show("Enjoy your coffee");
+            }
+            else if (double.Parse(TBYourMoney.Text) > double.Parse(FullCost.Text))
+            {
+                MessageBox.Show($"Enjoy your coffee, your change ${double.Parse(TBYourMoney.Text)- double.Parse(FullCost.Text)}");
+            }
+            TBYourMoney.Text = "0";
+            FullCost.Text = "0";
+            TBCount1cent.Text = "0";
+            TBCount5cent.Text = "0";
+            TBCount25cent.Text = "0";
+            TBCount.Text = "0";
+            ISelectedCoffee.Source = ISelectedCoffee.Source = new BitmapImage(new Uri("/Resources/CoffeeBeans.png", UriKind.RelativeOrAbsolute));
+            TBName.Text = "Выберите кофе";
+            TBCost.Text = "";
         }
     }
 }
